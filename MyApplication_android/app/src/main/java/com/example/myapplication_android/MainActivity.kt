fd5b045dae6e9f5d95f137340f7ca6265d38e89f
extendo0 <128452674+extendo0@users.noreply.github.com>
2024-09-24 13:47:17 +0600

    fix

--- MyApplication_android/app/src/main/java/com/example/myapplication_android/MainActivity.kt
+++ MyApplication_android/app/src/main/java/com/example/myapplication_android/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplication_androidTheme {
-                // Aaaa surface container using the 'background' color from the theme
+                // Aaawewerwea surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Greeting("Android")
                 }
